9f261a947a69965a3761475f2f6c598b19f7c5a5
A. Brooks Nagel <abrooksnagel@gmail.com>
2016-06-29 16:45:03 -0500

    add images, styling

--- client/client.js
+++ client/client.js
@@ -35,7 +35,26 @@ app.controller('HomeController', ['$scope', function($scope) {
       {image: 'images/IMG_5501.JPG', description: 'Image 03'},
       {image: 'images/Melissa_2.jpg', description: 'Image 04'}
   ];
+  //((((((((((((((((((((((((()))))))))))))))))))))))))
+  //             this code is untested
+  //((((((((((((((((((((((((()))))))))))))))))))))))))
+  $scope.currentIndex = 0;
+    $scope.setCurrentSlideIndex = function (index) {
+        $scope.currentIndex = index;
+    };
+    $scope.isCurrentSlideIndex = function (index) {
+        return $scope.currentIndex === index;
+    };
+    $scope.prevSlide = function () {
+        $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+    };
+    $scope.nextSlide = function () {
+        $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+    };
+
 }]);
+
+
 
 app.controller('AboutController', ['$scope', function($scope) {
 
@@ -48,3 +67,24 @@ app.controller('WorkController', ['$scope', function($scope) {
 app.controller('ShopController', ['$scope', function($scope) {
 
 }]);
+
+app.animation('.slide-animation', function () {
+        return {
+            addClass: function (element, className, done) {
+                if (className == 'ng-hide') {
+                    TweenMax.to(element, 0.5, {left: -element.parent().width(), onComplete: done });
+                } else {
+                    done();
+                }
+            },
+            removeClass: function (element, className, done) {
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
+                TweenMax.set(element, { left: element.parent().width() });
+                TweenMax.to(element, 0.5, {left: 0, onComplete: done });
+                } else {
+                done();
+                }
+              }
+        };
+});
